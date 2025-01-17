32a8ff0195e0a4cad0c44241238c97bb8e63c939
SafaSalam <safa16salam@gmail.com>
2025-01-17 06:50:15 +0500

    new feature added

--- main.py
+++ main.py
@@ -5,3 +5,13 @@ import pandas as pd
 def connect_db():
     conn = sqlite3.connect('farm.db')
     return conn
+
+def add_animal(animal_id, weight, cost, date):
+    conn = connect_db()
+    cursor = conn.cursor()
+    cursor.execute('''
+        INSERT INTO animals (animal_id, initial_weight, initial_cost, purchase_date)
+        VALUES (?, ?, ?, ?)
+    ''', (animal_id, weight, cost, date))
+    conn.commit()
+    conn.close()
